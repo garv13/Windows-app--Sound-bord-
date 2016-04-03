08defb803003c5aee45fd1681918faa6e9873136
garv13 <garvjasuja@outlook.com>
2016-04-03 17:36:31 +0530

    version 1

---
+++ soundboard/RecordAudio.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Coding4Fun.Toolkit.Audio;
+using Coding4Fun.Toolkit.Audio.Helpers;
+using Coding4Fun.Toolkit.Controls;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Newtonsoft.Json;
+using SoundBoard.Resources;
+using SoundBoard.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace SoundBoard
+{
+
+    public partial class RecordAudio : PhoneApplicationPage
+    {
+        private MicrophoneRecorder _recorder = new MicrophoneRecorder();
+        private IsolatedStorageFileStream _audioStream;
+        private string _tempFileName = "tempWav.wav";
+
+        public RecordAudio()
+        {
+            InitializeComponent();
+
+            BuildLocalizedApplicationBar();
+
+        }
+
+        private void BuildLocalizedApplicationBar()
+        {
+            ApplicationBar = new ApplicationBar();
+            ApplicationBarIconButton recordAudioAppBar = new ApplicationBarIconButton();
+
+            recordAudioAppBar.IconUri = new Uri("/Assets/AppBar/save.png", UriKind.Relative);
+            recordAudioAppBar.Text = AppResources.AppBarSave;
+
+            recordAudioAppBar.Click +=recordAudioAppBar_Click;
+            ApplicationBar.Buttons.Add(recordAudioAppBar);
+            ApplicationBar.IsVisible = false;
+        }
+
+        private void recordAudioAppBar_Click(object sender, EventArgs e)
+        {
+
+            InputPrompt filename = new InputPrompt();
+
+            filename.Title = "Sound Name";
+            filename.Message = "What should We call the sound?";
+
+            filename.Completed += FileNameCompleted;
+
+            filename.Show();
+        }
+
+        private void FileNameCompleted(object sender, PopUpEventArgs<string, PopUpResult> e)
+        {
+            
+            if(e.PopUpResult == PopUpResult.Ok)
+            {
+                //Create a sound data object
+
+                SoundData soundData = new SoundData();
+                soundData.FilePath = string.Format("/customAudio/{0}.wav",DateTime.Now.ToFileTime());
+                soundData.Title = e.Result;
+                    
+                // save the wav file in directory/customaudio
+                using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetUserStoreForApplication()){
+
+                    if(!isoStore.DirectoryExists("/customAudio/"))
+                        isoStore.CreateDirectory("/customAudio/");
+
+                    isoStore.MoveFile(_tempFileName, soundData.FilePath);
+
+                }
+
+                // add sound data object to app.viewmodel.customsounds
+
+                App.ViewModel.CustomSounds.Items.Add(soundData);
+
+                // save the list of custom sounds to IsolatedStorage.ApplicationSettings
+
+                var data = JsonConvert.SerializeObject(App.ViewModel.CustomSounds);
+
+                IsolatedStorageSettings.ApplicationSettings[SoundModel.CustomSoundsKey] = data;
+                IsolatedStorageSettings.ApplicationSettings.Save();
+
+                // We need to modify our sound model to retrive custom sounds from IsolatedStorage.ApplicationSettings
+
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+
+            }
+        }
+
+        private void RecordAudioChecked(object sender, RoutedEventArgs e)
+        {
+            PlayAudio.IsEnabled = false;
+            ApplicationBar.IsVisible = false;
+            RotateCircle.Begin();
+            _recorder.Start();
+        }
+
+        private void RecordAudioUnchecked(object sender, RoutedEventArgs e)
+        {
+            _recorder.Stop();
+            SaveTempAudio(_recorder.Buffer);
+            PlayAudio.IsEnabled = true;
+            ApplicationBar.IsVisible = true;
+            RotateCircle.Stop();
+        }
+        private void SaveTempAudio(MemoryStream buffer)
+        {
+            //be Defensive ...trust no one
+            if (buffer == null)
+                throw new ArgumentException("Atttempting to save an empty sound buffer");
+
+            //Clean up the audio player and hold on our audio stream
+            if(_audioStream!=null)
+            {
+                AudioPlayer.Stop();
+                AudioPlayer.Source = null;
+
+                _audioStream.Dispose();
+            }
+
+            var bytes = buffer.GetWavAsByteArray(_recorder.SampleRate);
+            
+            using(IsolatedStorageFile isostore = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+
+                if (isostore.FileExists(_tempFileName))
+                    isostore.DeleteFile(_tempFileName);
+
+                _tempFileName = string.Format("{0}.wav", DateTime.Now.ToFileTime());
+                
+                _audioStream = isostore.CreateFile(_tempFileName);
+                _audioStream.Write(bytes, 0, bytes.Length);
+                //Play ... Set Source of Media Element
+                AudioPlayer.SetSource(_audioStream);
+            }
+        }
+
+        private void PlayAudioClick(object sender, RoutedEventArgs e)
+        {
+
+            AudioPlayer.Play();
+        }
+    }
+}
---
+++ soundboard/ViewModels/SoundModel.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.ViewModels
+{
+  public class SoundModel
+    {
+        public SoundGroup CustomSounds { get; set; }
+        public SoundGroup Animals { get; set; }
+        public SoundGroup Taunts { get; set; }
+        public SoundGroup Warnings { get; set; }
+
+        public bool IsDataLoaded { get; set; }
+
+        public const string CustomSoundsKey = "CustomSound";
+
+
+      public void LoadData()
+        {
+          //Load Data Into the model    
+
+            Animals = CreateAnimalsGroup();
+            Taunts = CreateTauntsGroup();
+            Warnings = CreateWarningsGroup();
+
+            CustomSounds = LoadCustomSounds();
+
+          IsDataLoaded = true;
+        }
+
+      private SoundGroup LoadCustomSounds()
+      {
+
+          SoundGroup data;
+          string dataFromAppSettings;
+          if (IsolatedStorageSettings.ApplicationSettings.TryGetValue(CustomSoundsKey, out dataFromAppSettings))
+          {
+              data = JsonConvert.DeserializeObject<SoundGroup>(dataFromAppSettings);
+          }
+          else
+          {
+              data = new SoundGroup();
+              data.Title = "mine";
+          }
+
+          return data;
+      }
+
+      private SoundGroup CreateAnimalsGroup()
+      {
+          SoundGroup data = new SoundGroup();
+
+          data.Title = "Warfields";
+            string basePath = "assets/audio/warfield/";
+
+            data.Items.Add(new SoundData
+            { 
+                Title = "All Right Let's Do This", 
+                FilePath = basePath + "alright_lets_do_this.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            {
+                Title = "Bomb Has Been Defused", 
+                FilePath = basePath + "bombdef.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            {
+                Title = "bomb Has Been Planted", 
+                FilePath = basePath + "bombpl.mp3" 
+            });
+
+            data.Items.Add(new SoundData
+            {
+                Title = "Bomb Explode", 
+                FilePath = basePath + "c4_explode1.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            {
+                Title = "Affirmative", 
+                FilePath = basePath + "ct_affirm.mp3" 
+            });
+
+            data.Items.Add(new SoundData
+            {
+                Title = "Fire In The Hole", 
+                FilePath = basePath + "ct_fireinhole.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            { 
+                Title = "Headshot", 
+                FilePath = basePath + "headshot1.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            { 
+                Title = "Gun Fire", 
+                FilePath = basePath + "m3-1.mp3" 
+            });
+
+            data.Items.Add(new SoundData
+            { 
+                Title = "Man Down", 
+                FilePath = basePath + "pl_die1.mp3" 
+            });
+
+            data.Items.Add(new SoundData 
+            { 
+                Title = "Roger That", 
+                FilePath = basePath + "roger_that.mp3"
+            });
+
+            data.Items.Add(new SoundData 
+            { 
+                Title = "Sound Like A Plan", 
+                FilePath = basePath + "soundslikeaplan-2.mp3"
+            });
+                    
+            return data;
+        }
+
+      private SoundGroup CreateTauntsGroup()
+        {
+            SoundGroup data = new SoundGroup();
+            data.Title = "taunts";
+            string basePath = "assets/audio/taunts/";
+
+            data.Items.Add(new SoundData { Title = "Come to Papa", 
+                FilePath = basePath + "come_to_papa.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Game Begins", 
+                FilePath = basePath + "game_begins-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "MayBe..", 
+                FilePath = basePath + "maybe-2.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Negative -", 
+                FilePath = basePath + "negative.mp3" });
+
+            data.Items.Add(new SoundData { Title = "OH. Man", 
+                FilePath = basePath + "oh_man.mp3" });
+
+            data.Items.Add(new SoundData { Title = "I Am Ahead of You", 
+                FilePath = basePath + "slow_ahead-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Stayout", 
+                FilePath = basePath + "stayout-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Is it Supposed To Be Hurt", 
+                FilePath = basePath + "supposed_hurt-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "That's the Way It Is Done", 
+                FilePath = basePath + "thats_the_way_this_is_done.mp3" });
+
+            data.Items.Add(new SoundData { Title = "They Never Knew What Hit Them", 
+                FilePath = basePath + "they_never_knew_what_hit_them.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Uhhh", 
+                FilePath = basePath + "uhhh-2.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Yesss", 
+                FilePath = basePath + "yes-3.mp3" });
+
+            return data;
+        }
+
+      private SoundGroup CreateWarningsGroup()
+        {
+            SoundGroup data = new SoundGroup();
+            data.Title = "Warnings";
+            string basePath = "assets/audio/warnings/";
+
+            data.Items.Add(new SoundData { Title = "Attack", 
+                FilePath = basePath + "attack-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Bwahahaha", 
+                FilePath = basePath + "bwahaha-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "City Is Going Down", 
+                FilePath = basePath + "city_down-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Have You Fixed Your Problem",
+                FilePath = basePath + "fixed_problem-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Get Em", 
+                FilePath = basePath + "get_em-2.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Gonna Boooom", 
+                FilePath = basePath + "gonna_boom-2.mp3" });
+
+            data.Items.Add(new SoundData { Title = "It Means War", 
+                FilePath = basePath + "means_war-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "You Need That", 
+                FilePath = basePath + "need_that-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Is It Over??", 
+                FilePath = basePath + "over_yet-4.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Prepared To Be Crushed", 
+                FilePath = basePath + "prepare_crushed-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Stayout", 
+                FilePath = basePath + "stayout-1.mp3" });
+
+            data.Items.Add(new SoundData { Title = "This Is My House", 
+                FilePath = basePath + "this_is_my_house.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Victory Is Mine",
+                FilePath = basePath + "victoryismine-3.mp3" });
+
+            data.Items.Add(new SoundData { Title = "Wanna Bet??",
+                FilePath = basePath + "wanna_bet-2.mp3" });
+            data.Items.Add(new SoundData { Title = "Wanna Give Up??", 
+                FilePath = basePath + "wanngiveup-2.mp3" });
+            return data;
+        }
+ 
+    }
+}
